1f7839b06f561581e71eeeee5d59beef6de39891
Misha <mishakarpovan@mail.ru>
2018-01-17 20:47:24 +0200

    new

---
+++ TtlChanger/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Xml.XPath;
+
+namespace TtlChanger
+{
+    class Program
+    {
+        private const string fileConfig = "config.xml";
+        private const int rebootTimeOutS = 15;
+
+        static void Main(string[] args)
+        {
+            if (File.Exists(fileConfig))
+            {
+                // Load configuration
+                XPathDocument xml = new XPathDocument(fileConfig);
+                XPathNavigator nav = xml.CreateNavigator();
+
+                if (nav.SelectSingleNode("xml/config/ip") != null) Telnet.Ip = nav.SelectSingleNode("xml/config/ip").Value;
+                if (nav.SelectSingleNode("xml/config/port") != null) Telnet.Port = int.Parse(nav.SelectSingleNode("xml/config/port").Value);
+                if (nav.SelectSingleNode("xml/config/login") != null) Telnet.Login = nav.SelectSingleNode("xml/config/login").Value;
+                if (nav.SelectSingleNode("xml/config/password") != null) Telnet.Password = nav.SelectSingleNode("xml/config/password").Value;
+
+                Console.WriteLine("Loaded configuration from {0}", fileConfig);
+            }
+            else Console.WriteLine("Not found {0}, used default configuration", fileConfig);
+
+            Console.WriteLine("Ip: {0}", Telnet.Ip);
+            Console.WriteLine("Port: {0}", Telnet.Port);
+            Console.WriteLine("Login: {0}", Telnet.Login);
+            Console.WriteLine("Password: {0}", Telnet.Password);
+
+            if (args.Length != 0)
+            {
+                if (args[0] == "reboot") Reboot();
+                else ChangeTtl();
+            }
+            else
+            {
+                Console.WriteLine("Press key 'R' to reboot router or any another key to change TTL.");
+                if (Console.ReadKey(true).Key == ConsoleKey.R) Reboot();
+                else ChangeTtl();
+            }
+
+        }
+
+
+       private static void ChangeTtl()
+        {
+            Console.WriteLine("TTL changing...");
+            // Firewall command for set TTL
+            Telnet.Send("iptables -t mangle -I POSTROUTING -o \"${WAN_IF}\" -j TTL --ttl-set 128");
+            Telnet.Send("iptables -I PREROUTING -t mangle -d 8.8.8.8 -j TTL --ttl-set 128");
+            Console.WriteLine("TTL changed!");
+        }
+
+        private static void Reboot()
+        {
+            Console.WriteLine("Wait rebooting...");
+
+            Telnet.Send("reboot");
+            Telnet.Close();
+            // If success than connection true, so next exceptions will be depended on booting device
+
+            System.Threading.Thread.Sleep(rebootTimeOutS*1000);
+            while (!Telnet.IsConnected)
+            {
+                try
+                {
+                    Telnet.Connect();
+                }
+                catch
+                {
+                    // wait when router boot 
+                    System.Threading.Thread.Sleep(1000);
+                }
+
+            }
+            Console.WriteLine("Successful reboot!");
+
+            ChangeTtl();
+
+        }
+    }
+}
---
+++ TtlChanger/Telnet.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TtlChanger
+{
+    static class Telnet
+    {
+
+        private static TcpClient Connection;
+        private static int TimeOutMs = 500;
+
+        public static string Ip { get; set; } = "192.168.1.1";
+        public static int Port { get; set; } = 23;
+        public static string Login { get; set; } = "admin";
+        public static string Password { get; set; } = "admin";
+
+        public static void Connect()
+        {
+            Connection = new TcpClient(Ip,Port);
+            System.Threading.Thread.Sleep(TimeOutMs);
+            Send(Login);
+            Send(Password);
+            string s = Read();
+            if (s.TrimEnd().EndsWith(":"))
+                throw new Exception("Failed to connect : Login incorrect");
+
+        }
+
+        public static void Close()
+        {
+            if (Connection != null) Connection.Close();
+        }
+
+        public static bool IsConnected
+        {
+            get { return Connection.Connected; }
+        }
+
+        public static void Send(string cmd)
+        {
+            if (Connection==null || !Connection.Connected) Connect();
+            byte[] buf = System.Text.ASCIIEncoding.ASCII.GetBytes(cmd+"\r\n");
+            Connection.GetStream().Write(buf, 0, buf.Length);
+            System.Threading.Thread.Sleep(TimeOutMs);
+        }
+
+
+        public static string Read()
+        {
+            if (!Connection.Connected) return null;
+            StringBuilder sb = new StringBuilder();
+            do
+            {
+                ParseTelnet(sb);
+                System.Threading.Thread.Sleep(TimeOutMs);
+            } while (Connection.Available > 0);
+            return sb.ToString();
+        }
+
+        enum Verbs
+        {
+            WILL = 251,
+            WONT = 252,
+            DO = 253,
+            DONT = 254,
+            IAC = 255
+        }
+
+        enum Options
+        {
+            SGA = 3
+        }
+
+
+
+        private static void ParseTelnet(StringBuilder sb)
+        {
+            while (Connection.Available > 0)
+            {
+                int input = Connection.GetStream().ReadByte();
+                switch (input)
+                {
+                    case -1:
+                        break;
+                    case (int)Verbs.IAC:
+                        // interpret as command
+                        int inputverb = Connection.GetStream().ReadByte();
+                        if (inputverb == -1) break;
+                        switch (inputverb)
+                        {
+                            case (int)Verbs.IAC:
+                                //literal IAC = 255 escaped, so append char 255 to string
+                                sb.Append(inputverb);
+                                break;
+                            case (int)Verbs.DO:
+                            case (int)Verbs.DONT:
+                            case (int)Verbs.WILL:
+                            case (int)Verbs.WONT:
+                                // reply to all commands with "WONT", unless it is SGA (suppres go ahead)
+                                int inputoption = Connection.GetStream().ReadByte();
+                                if (inputoption == -1) break;
+                                Connection.GetStream().WriteByte((byte)Verbs.IAC);
+                                if (inputoption == (int)Options.SGA)
+                                    Connection.GetStream().WriteByte(inputverb == (int)Verbs.DO ? (byte)Verbs.WILL : (byte)Verbs.DO);
+                                else
+                                    Connection.GetStream().WriteByte(inputverb == (int)Verbs.DO ? (byte)Verbs.WONT : (byte)Verbs.DONT);
+                                Connection.GetStream().WriteByte((byte)inputoption);
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    default:
+                        sb.Append((char)input);
+                        break;
+                }
+            }
+        }
+
+
+    }
+}
